61b4655ee051364fe8f1cb5214ea7e0a8dcaddca
Markus <markblake190399@gmail.com>
2023-05-12 01:17:07 +0200

    Es una Tarea de Ende

---
+++ ejemplo_operaciones.py
@@ -0,0 +1,8 @@
+def suma(a):
+    return a+a
+
+def multplicacion(a):
+    return a*a
+
+def elevar_exponente(a):
+    return a**a
---
+++ ejemplo_operaciones2.py
@@ -0,0 +1,5 @@
+from ejemplo_operaciones import suma as sum, multplicacion as multi, elevar_exponente as exponente
+a=int(input("Introduce un numero: "))
+print(sum(a))
+print(multi(a))
+print(exponente(a))
